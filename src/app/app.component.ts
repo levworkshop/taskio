a6401baade3550fde9eb990bc005d575cab340da
levworkshop <noreply@githubusers.com>
2023-02-20 20:44:08 +0200

    data binding - value and property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'taskio';
+    title = 'Task IO';
+
+    getImagePath(): string {
+        return "https://cdn.pixabay.com/photo/2023/02/06/01/14/superb-fairywren-7770904__340.jpg";
+    }
+
+    getTaskById(id: number): string {
+        const tasks: Array<string> = [
+            'Your first task',
+            'A second task',
+            'Another task'
+        ]
+
+        return tasks[id];
+    }
 }
