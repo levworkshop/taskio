f839b03b61cc1a03ffd7eceb6e29bce2940b576e
levworkshop <noreply@githubusers.com>
2023-03-02 17:40:48 +0200

    about module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { AboutPageComponent } from './about-page/about-page.component';
 import { ProjectsModule } from './projects/projects.module';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        FooterComponent,
-        AboutPageComponent
+        FooterComponent
     ],
     imports: [
         BrowserModule,
