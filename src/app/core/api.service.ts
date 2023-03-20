86ea273315ec3b29abe335c0b53f4a658e0739f3
levworkshop <noreply@githubusers.com>
2023-03-20 18:50:31 +0200

    api common in a separate method

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Task } from '../app.component';
+import { Project, Task } from '../app.component';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
+
+    serverUrl = 'http://localhost:3000/'
 
     constructor(private http: HttpClient) { }
 
@@ -15,27 +17,45 @@ export class ApiService {
     }
 
     getTasks(): Observable<Array<Task>> {
-        return this.http.get<Array<Task>>('http://localhost:3000/tasks')
+        return this.http.get<Array<Task>>(`${this.serverUrl}tasks`)
     }
 
     addTask(task: Task): Observable<Task> {
-        return this.http.post<Task>(
-            'http://localhost:3000/tasks',
-            task,
+        // return this.http.post<Task>(
+        //     `${this.serverUrl}tasks`,
+        //     task,
+        //     { headers: { 'Content-Type': 'application/json' } }
+        // )
+        return this.POST<Task>('tasks', task);
+    }
+
+    addProject(project: Project): Observable<Project> {
+        // return this.http.post<Project>(
+        //     `${this.serverUrl}projects`,
+        //     project,
+        //     { headers: { 'Content-Type': 'application/json' } }
+        // )
+        return this.POST<Project>('projects', project);
+    }
+
+    POST<T>(endpoint: string, data: T): Observable<T> {
+        return this.http.post<T>(
+            `${this.serverUrl}${endpoint}`,
+            data,
             { headers: { 'Content-Type': 'application/json' } }
         )
     }
 
     deleteTask(id: string): Observable<Task> {
         return this.http.delete<Task>(
-            `http://localhost:3000/tasks/${id}`,
+            `${this.serverUrl}tasks/${id}`,
             { headers: { 'Content-Type': 'application/json' } }
         )
     }
 
     updateTask(id: string, task: Task): Observable<Task> {
         return this.http.put<Task>(
-            `http://localhost:3000/tasks/${id}`,
+            `${this.serverUrl}tasks/${id}`,
             task,
             { headers: { 'Content-Type': 'application/json' } }
         )
